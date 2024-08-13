31646477e563deabcbc834bdf27c3f71723fb838
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-13 18:18:52 +0530

    automated commit #3238

--- shell-script/01-functions.sh
+++ shell-script/01-functions.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 USERID=$(id -u)
+SCRIPTNAME=$(echo $0 | cut -d "." -f )
+LOGFILE=/tmp/$SCRIPTNAME.log
+
+W="\e[0m"
+R="\e[31m"
+G="\e[32m"
+
+VALIDAATE(){
+  if [ $1 -ne 0 ];
+  then
+    echo "$2 ...$R FAILURE $W"
+    exit 1
+  else
+    echo "$2 ...$R SUCCESS $W"
+  fi
+}
+
 
 if [ $USERID -ne 0 ];
 then
@@ -10,3 +27,8 @@ else
   echo "your a supper user"
 fi
 
+dnf install git -y &>>$LOGFILE
+VALIDAATE $? "installing git"
+
+dnf remove git -y  &>>$LOGFILE
+VALIDAATE $? "installing git"
