85cc29308d84b31459d9204a0c3ee6ab5b2d5cb2
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-13 18:56:42 +0530

    automated commit #3250

--- shell-script/01-functions.sh
+++ shell-script/01-functions.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
+# Check if the script is being run as root
 ID=$(id -u)
 if [ $ID -ne 0 ]; then
-  echo You should run this script as root user or with sudo privileges.
+  echo "You should run this script as root user or with sudo privileges."
   exit 1
 fi
 
+# Function to check the status of the last executed command
 StatusCheck() {
   if [ $1 -eq 0 ]; then
-    echo -e Status = "\e[32mSUCCESS\e[0m"
+    echo -e "Status = \e[32mSUCCESS\e[0m"
   else
-    echo -e Status = "\e[31mFAILURE\e[0m"
+    echo -e "Status = \e[31mFAILURE\e[0m"
     exit 1
   fi
 }
 
-#SCRIPT_NAME=$(echo $0 | cut -d "." -f )
+# Define log file location
 LOGFILE=/tmp/mysql.log
 
-echo "installing mysql"
+# Install MySQL and check the status
+echo "Installing MySQL"
 dnf install mysql -y &>>$LOGFILE
 StatusCheck $?
 
-
+# Remove MySQL and check the status
 echo "Removing MySQL"
 yum remove mysqlll -y &>>$LOGFILE
 StatusCheck $?
