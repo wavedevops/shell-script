c80c64d098dc710b54ba72c0d979d707bba6f5fc
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-13 18:32:35 +0530

    automated commit #3239

--- shell-script/01-functions.sh
+++ shell-script/01-functions.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
-
+#ID=$(id -u)
+#if [ $ID -ne 0 ]; then
+#  echo You should run this script as root user or with sudo privileges.
+#  exit 1
+#fi
+#
+#StatusCheck() {
+#  if [ $1 -eq 0 ]; then
+#    echo -e Status = "\e[32mSUCCESS\e[0m"
+#  else
+#    echo -e Status = "\e[31mFAILURE\e[0m"
+#    exit 1
+#  fi
+#}
 USERID=$(id -u)
 SCRIPTNAME=$(echo $0 | cut -d "." -f )
 LOGFILE=/tmp/$SCRIPTNAME.log
 
-W="\e[0m"
-R="\e[31m"
-G="\e[32m"
-
 VALIDAATE(){
-  if [ $1 -ne 0 ];
-  then
-    echo "$2 ...$R FAILURE $W"
-    exit 1
+  if [ $1 -eq 0 ]; then
+    echo -e Status = "\e[32mSUCCESS\e[0m"
   else
-    echo "$2 ...$R SUCCESS $W"
+    echo -e Status = "\e[31mFAILURE\e[0m"
+    exit 1
   fi
 }
-
-
 if [ $USERID -ne 0 ];
 then
   echo "your a not sudo privilege"
@@ -27,8 +33,12 @@ else
   echo "your a supper user"
 fi
 
+echo "installing git"
 dnf install git -y &>>$LOGFILE
-VALIDAATE $? "installing git"
+VALIDAATE $?
 
-dnf remove git -y  &>>$LOGFILE
-VALIDAATE $? "installing git"
+echo "removeing git"
+yum remove git -y  &>>$LOGFILE
+VALIDAATE $?
+
+}
