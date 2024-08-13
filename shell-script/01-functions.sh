7c73853666f2efa99d4e005888e76840aa850d51
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-13 19:01:03 +0530

    automated commit #3252

--- shell-script/01-functions.sh
+++ shell-script/01-functions.sh
@@ -9,7 +9,7 @@ fi
 
 # Function to check the status of the last executed command
 StatusCheck() {
-  if [ $1 -ne 0 ]; then
+  if [ $1 -eq 0 ]; then
     echo -e "Status = \e[32mSUCCESS\e[0m"
   else
     echo -e "Status = \e[31mFAILURE\e[0m"
