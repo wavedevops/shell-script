45e115c288eb848ec794eb72416f35fc2d774fd3
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-13 19:20:27 +0530

    automated commit #3264

--- shell-script/01-functions.sh
+++ shell-script/01-functions.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 
-# Check if the script is being run as root
-ID=$(id -u)
-if [ $ID -ne 0 ]; then
-  echo "You should run this script as root user or with sudo privileges."
-  exit 1
-fi
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-# Function to check the status of the last executed command
-StatusCheck() {
-  if [ $1 -eq 0 ]; then
-    echo -e "Status = \e[32mSUCCESS\e[0m"
-  else
-    echo -e "Status = \e[31mFAILURE\e[0m"
-    exit 1
-  fi
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
 }
 
-# Define log file location
-LOGFILE=/tmp/mysql.log
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
 
-# Install MySQL and check the status
-echo "Installing nginx"
-dnf install nginx -y &>>$LOGFILE
-StatusCheck $?
-
-echo "sleep 30 seconds"
-sleep 10
-StatusCheck $?
-
-# Remove MySQL and check the status
-echo "Removing nginx "
-yum remove nginxxxxxg &>>$LOGFILE
-StatusCheck $?
+for i in $@
+do
+    echo "package to install: $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo -e "$i already installed...$Y SKIPPING $N"
+    else
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
+    fi
+done
