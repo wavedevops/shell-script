e4a94ef55ff95f967648a876ae92d93c6eb5a83c
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-15 17:18:31 +0530

    automated commit #3291

--- common.sh
+++ common.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 
-# Get the current user ID
 USERID=$(id -u)
-# Check if the user is root (user ID 0)
-if [ "$USERID" -ne 0 ]; then
-  echo "Please run this script with root access."
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+echo "Please enter DB password:"
+read  mysql_root_password
+# -s
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+  echo -e "$2...$R FAILURE $N"
   exit 1
+else
+  echo -e "$2...$G SUCCESS $N"
+fi
+}
+
+if [ $USERID -ne 0 ]
+then
+  echo "Please run this script with root access."
+  exit 1 # manually exit if error comes.
 else
   echo "You are super user."
 fi
-
-
-
-# Define color codes for output
-R="\e[31m"   # Red
-G="\e[32m"   # Green
-Y="\e[33m"   # Yellow
-N="\e[0m"    # Reset color (No color)
-
-# Now you can use these variables and color codes in your script
-
-
-# Define the VALIDATE function
-VALIDATE(){
-  if [ "$1" -ne 0 ]; then
-    echo -e "$2...$R FAILURE $N"
-    exit 1
-  else
-    echo -e "$2...$G SUCCESS $N"
-  fi
-}
-
--- db.sh
+++ db.sh
@@ -1,34 +1,6 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-echo "Please enter DB password:"
-read  mysql_root_password
-# -s
-VALIDATE(){
-if [ $1 -ne 0 ]
-then
-  echo -e "$2...$R FAILURE $N"
-  exit 1
-else
-  echo -e "$2...$G SUCCESS $N"
-fi
-}
-
-if [ $USERID -ne 0 ]
-then
-  echo "Please run this script with root access."
-  exit 1 # manually exit if error comes.
-else
-  echo "You are super user."
-fi
-
+source common.sh
 
 dnf install mysql-server -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL Server"
@@ -43,7 +15,7 @@ VALIDATE $? "Starting MySQL Server"
 # VALIDATE $? "Setting up root password"
 
 #Below code will be useful for idempotent nature
-mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h 172.31.94.9 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
