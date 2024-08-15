cc739ec63422a31ee2cbc663cbeecbd5ce5b790d
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-15 19:04:48 +0530

    automated commit #3309

--- frontend.sh
+++ frontend.sh
@@ -11,22 +11,21 @@ VALIDATE $? "Enabling nginx"
 systemctl start nginx &>>$LOGFILE
 VALIDATE $? "Starting nginx"
 
-rm -rf /usr/share/nginx/html/* &>>$LOGFILE
-VALIDATE $? "Removing existing content"
-
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
-VALIDATE $? "Downloading frontend code"
-
-cd /usr/share/nginx/html &>>$LOGFILE
-unzip /tmp/frontend.zip &>>$LOGFILE
-VALIDATE $? "Extracting frontend code"
-
-#check your repo and path
-#cp /home/ec2-user/shell-script/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
-#VALIDATE $? "Copied expense conf"
-
-cp expense.conf /etc/nginx/default.d/expense.conf
-VALIDATE $? "Copied expense conf"
-
-systemctl restart nginx &>>$LOGFILE
-VALIDATE $? "Restarting nginx"
+#rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+#VALIDATE $? "Removing existing content"
+#
+#curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+#VALIDATE $? "Downloading frontend code"
+#
+#cd /usr/share/nginx/html &>>$LOGFILE
+#unzip /tmp/frontend.zip &>>$LOGFILE
+#VALIDATE $? "Extracting frontend code"
+#
+##check your repo and path
+##cp /home/ec2-user/shell-script/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+##VALIDATE $? "Copied expense conf"
+#
+#
+#
+#systemctl restart nginx &>>$LOGFILE
+#VALIDATE $? "Restarting nginx"
