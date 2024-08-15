217f5db7cc37b328353becffb1f83d30405c6b72
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-15 11:03:33 +0530

    automated commit #3290

--- db.sh
+++ db.sh
@@ -1,57 +1,54 @@
 #!/bin/bash
 
-# Get the current user ID
 USERID=$(id -u)
-# Check if the user is root (user ID 0)
-if [ "$USERID" -ne 0 ]; then
-  echo "Please run this script with root access."
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+echo "Please enter DB password:"
+read  mysql_root_password
+# -s
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+  echo -e "$2...$R FAILURE $N"
   exit 1
+else
+  echo -e "$2...$G SUCCESS $N"
+fi
+}
+
+if [ $USERID -ne 0 ]
+then
+  echo "Please run this script with root access."
+  exit 1 # manually exit if error comes.
 else
   echo "You are super user."
 fi
 
 
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Server"
 
-# Define color codes for output
-R="\e[31m"   # Red
-G="\e[32m"   # Green
-Y="\e[33m"   # Yellow
-N="\e[0m"    # Reset color (No color)
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling MySQL Server"
 
-# Now you can use these variables and color codes in your script
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting MySQL Server"
 
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# VALIDATE $? "Setting up root password"
 
-# Define the VALIDATE function
-VALIDATE(){
-  if [ "$1" -ne 0 ]; then
-    echo -e "$2...$R FAILURE $N"
-    exit 1
-  else
-    echo -e "$2...$G SUCCESS $N"
-  fi
-}
-
-
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
-dnf install mysql-server -y &>>LOGFILE
-VALIDATE $? "install mysql"
-
-systemctl enable mysqld &>>LOGFILE
-VALIDATE $? "enable mysql server"
-
-systemctl start mysqld &>>LOGFILE
-VALIDATE $? "start mysql server"
-
-#mysql_secure_installation --set-root-pass ExpenseApp@1
-#VALIDATE $? "setup mysql server root password"
-
-mysql -h 172.31.94.9 -uroot -pExpenseApp@1 -e 'show databases;' &>>LOGFILE
-if [ $? -eq 0 ]; then
-  echo -e "$Y mysql already exists $N"
+#Below code will be useful for idempotent nature
+mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
 else
-  mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
-  VALIDATE $? "setup mysql root password"
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
+#  mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
