3da8e23648ef717e488a20e99fc5d8d814f7d12e
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-15 18:32:56 +0530

    automated commit #3300

--- frontend.sh
+++ frontend.sh
@@ -2,19 +2,19 @@
 
 source common.sh
 
-dnf install nginx -y
+dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "install nginx"
 
-systemctl enable nginx
+systemctl enable nginx &>>$LOGFILE
 VALIDATE $? "enable nginx"
 
-systemctl start nginx
+systemctl start nginx &>>$LOGFILE
 VALIDATE $? "start nginx"
 
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
 VALIDATE $? "remove old html file "
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
 VALIDATE $? "download frontend zip"
 
 cd /usr/share/nginx/html &>>$LOGFILE
@@ -22,8 +22,9 @@ unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code"
 
 #check your repo and path
-cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+cp /home/ec2-user/shell-script/backend.service /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE $? "Copied expense conf"
 
 
-systemctl restart nginx
+systemctl restart nginx &>>$LOGFILE
+VALIDATE $? "Restarted nginx"
