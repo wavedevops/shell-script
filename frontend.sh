e991595954d90392469307b14b6ae1f480ac8ff0
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-15 18:49:41 +0530

    automated commit #3306

--- frontend.sh
+++ frontend.sh
@@ -3,35 +3,6 @@
 source common.sh
 
 dnf install nginx -y &>>$LOGFILE
-VALIDATE $? "install nginx"
-
-systemctl enable nginx &>>$LOGFILE
-VALIDATE $? "enable nginx"
-
-systemctl restart nginx &>>$LOGFILE
-VALIDATE $? "restart nginx"
-
-rm -rf /usr/share/nginx/html/* &>>$LOGFILE
-VALIDATE $? "remove old html file "
-
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
-VALIDATE $? "download frontend zip"
-
-cd /usr/share/nginx/html &>>$LOGFILE
-unzip /tmp/frontend.zip &>>$LOGFILE
-VALIDATE $? "Extracting frontend code"
-
-#check your repo and path
-cp /home/ec2-user/shell-script/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
-VALIDATE $? "Copied expense conf"
-
-systemctl enable nginx &>>$LOGFILE
-systemctl restart nginx &>>$LOGFILE
-VALIDATE $? "Restarted nginx"
-
-#systemctl status nginx
-
-dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "Installing nginx"
 
 systemctl enable nginx &>>$LOGFILE
