c19e601f5ae3957fba9a567a89b689819ecac087
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-15 18:35:29 +0530

    automated commit #3301

--- frontend.sh
+++ frontend.sh
@@ -5,8 +5,6 @@ source common.sh
 dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "install nginx"
 
-systemctl enable nginx &>>$LOGFILE
-VALIDATE $? "enable nginx"
 
 systemctl start nginx &>>$LOGFILE
 VALIDATE $? "start nginx"
@@ -22,9 +20,11 @@ unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code"
 
 #check your repo and path
-cp /home/ec2-user/shell-script/backend.service /etc/nginx/default.d/expense.conf &>>$LOGFILE
+cp /home/ec2-user/shell-script/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE $? "Copied expense conf"
 
-
+systemctl enable nginx &>>$LOGFILE
 systemctl restart nginx &>>$LOGFILE
 VALIDATE $? "Restarted nginx"
+
+systemctl status nginx
