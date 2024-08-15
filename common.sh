8a42ce5dae42929f7b7556fe079a95f7681548b9
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-15 10:46:13 +0530

    automated commit #3286

--- common.sh
+++ common.sh
@@ -2,15 +2,22 @@
 
 # Get the current user ID
 USERID=$(id -u)
+# Check if the user is root (user ID 0)
+if [ "$USERID" -ne 0 ]; then
+  echo "Please run this script with root access."
+  exit 1
+else
+  echo "You are super user."
+fi
 
 # Get the current timestamp in the format YYYY-MM-DD-HH-MM-SS
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
 # Get the script name without the extension
-SCRIPT_NAME=$(basename "$0" | cut -d "." -f1)
+SCRIPT_NAME=$(echo "$0" | cut -d "." -f1)
 
 # Set the log file path
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+LOGFILE=/tmp/$SCRIPT_NAME.log
 
 # Define color codes for output
 R="\e[31m"   # Red
@@ -19,7 +26,6 @@ Y="\e[33m"   # Yellow
 N="\e[0m"    # Reset color (No color)
 
 # Now you can use these variables and color codes in your script
-
 
 
 # Define the VALIDATE function
@@ -31,11 +37,3 @@ VALIDATE(){
     echo -e "$2...$G SUCCESS $N"
   fi
 }
-
-# Check if the user is root (user ID 0)
-if [ "$USERID" -ne 0 ]; then
-  echo "Please run this script with root access."
-  exit 1
-else
-  echo "You are super user."
-fi
