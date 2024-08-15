054c1f48fa02f41f4ef7ec261812f6b780632a28
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-15 10:34:28 +0530

    automated commit #3283

--- db.sh
+++ db.sh
@@ -3,19 +3,19 @@ source common.sh
 dnf install mysql-server -y &>>LOGFILE
 VALIDATE $? "install mysql"
 
-systemctl enable mysqld
+systemctl enable mysqld &>>LOGFILE
 VALIDATE $? "enable mysql server"
 
-systemctl start mysqld
+systemctl start mysqld &>>LOGFILE
 VALIDATE $? "start mysql server"
 
 #mysql_secure_installation --set-root-pass ExpenseApp@1
 #VALIDATE $? "setup mysql server root password"
 
-mysql -h 172.31.94.9 -uroot -pExpenseApp@1 -e 'show databases;'
+mysql -h 172.31.94.9 -uroot -pExpenseApp@1 -e 'show databases;' &>>LOGFILE
 if [ $? -eq 0 ]; then
-  echo "mysql already exists"
+  echo "$Y mysql already exists $N"
 else
-  mysql_secure_installation --set-root-pass ExpenseApp@1
+  mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
   VALIDATE $? "setup mysql root password"
 fi
