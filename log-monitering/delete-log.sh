5b56d840f77543aac1b12e2b4060abdc521c615b
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-18 11:30:22 +0530

    automated commit #3323

--- log-monitering/delete-log.sh
+++ log-monitering/delete-log.sh
@@ -16,13 +16,13 @@ FILES=$( find "$SOURCE_DIRECTORY" -name '*.log' -mtime +14)
 while IFS= read -r line
 do
   echo deleting file $line
-#  rm -rf $line
+  rm -rf $line
 done <<< $FILES
 
 
 
-touch -d "2024-08-01 00:00:00" backend.log
-touch -d "2024-08-01 00:00:00" mysql.log
-touch -d "2024-08-01 00:00:00" prasad.log
-touch -d "2024-08-01 00:00:00" hari.log
-touch -d "2024-08-01 00:00:00" chandra.log
+#touch -d "2024-08-01 00:00:00" backend.log
+#touch -d "2024-08-01 00:00:00" mysql.log
+#touch -d "2024-08-01 00:00:00" prasad.log
+#touch -d "2024-08-01 00:00:00" hari.log
+#touch -d "2024-08-01 00:00:00" chandra.log
