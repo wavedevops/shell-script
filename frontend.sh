afb9632bb5d5f8b3cb16e44b7db497c21c14d97c
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-23 14:19:19 +0530

    automated commit #3415

--- frontend.sh
+++ frontend.sh
@@ -53,7 +53,7 @@ VALIDATE $? "Extracting frontend code"
 #VALIDATE $? "Copied expense conf"
 
 ## centos
-cp /home/root/shell-script/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+cp /root/shell-script/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE $? "Copied expense conf"
 
 systemctl enable nginx &>>$LOGFILE
