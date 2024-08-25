bbb40c5d5abab4c80ef9c4e6d82bd327cb644a9a
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-25 07:51:41 +0530

    automated commit #3423

--- remove/mysql.sh
+++ remove/mysql.sh
@@ -1,40 +1,93 @@
 #!/bin/bash
 
+#LOGFILE=/var/log/mysql_remove.log
+#
+## Function to validate the success or failure of each command
+#VALIDATE() {
+#  if [ $1 -ne 0 ]; then
+#    echo "$2 - Failed" | tee -a $LOGFILE
+#    exit 1
+#  else
+#    echo "$2 - Success" | tee -a $LOGFILE
+#  fi
+#}
+#
+## Stopping MySQL Server
+#echo "Stopping MySQL Server..." | tee -a $LOGFILE
+#systemctl stop mysqld &>>$LOGFILE
+#VALIDATE $? "Stopping MySQL Server"
+#
+## Disabling MySQL Server
+#echo "Disabling MySQL Server..." | tee -a $LOGFILE
+#systemctl disable mysqld &>>$LOGFILE
+#VALIDATE $? "Disabling MySQL Server"
+#
+## Removing MySQL Server
+#echo "Removing MySQL Server and dependencies..." | tee -a $LOGFILE
+#dnf remove mysql-server -y &>>$LOGFILE
+#VALIDATE $? "Removing MySQL Server"
+#
+## Removing MySQL Data
+#echo "Removing MySQL data directories..." | tee -a $LOGFILE
+#rm -rf /var/lib/mysql /etc/my.cnf &>>$LOGFILE
+#VALIDATE $? "Removing MySQL data directories"
+#
+## Cleaning up unused packages
+#echo "Cleaning up unused packages..." | tee -a $LOGFILE
+#dnf autoremove -y &>>$LOGFILE
+#VALIDATE $? "Cleaning up unused packages"
+#
+#echo "MySQL Server removal completed successfully!" | tee -a $LOGFILE
+
+
+
 LOGFILE=/var/log/mysql_remove.log
 
-# Function to validate the success or failure of each command
+# Function to validate the execution of commands
 VALIDATE() {
-  if [ $1 -ne 0 ]; then
-    echo "$2 - Failed" | tee -a $LOGFILE
-    exit 1
+  if [ $1 -eq 0 ]; then
+    echo "$2 - SUCCESS" &>>$LOGFILE
   else
-    echo "$2 - Success" | tee -a $LOGFILE
+    echo "$2 - FAILED" &>>$LOGFILE
+    exit 1
   fi
 }
 
-# Stopping MySQL Server
-echo "Stopping MySQL Server..." | tee -a $LOGFILE
+# Stop MySQL service
 systemctl stop mysqld &>>$LOGFILE
 VALIDATE $? "Stopping MySQL Server"
 
-# Disabling MySQL Server
-echo "Disabling MySQL Server..." | tee -a $LOGFILE
+# Disable MySQL service
 systemctl disable mysqld &>>$LOGFILE
 VALIDATE $? "Disabling MySQL Server"
 
-# Removing MySQL Server
-echo "Removing MySQL Server and dependencies..." | tee -a $LOGFILE
-dnf remove mysql-server -y &>>$LOGFILE
-VALIDATE $? "Removing MySQL Server"
+# Remove MySQL packages
+dnf remove mysql-server mysql -y &>>$LOGFILE
+VALIDATE $? "Removing MySQL packages"
 
-# Removing MySQL Data
-echo "Removing MySQL data directories..." | tee -a $LOGFILE
-rm -rf /var/lib/mysql /etc/my.cnf &>>$LOGFILE
-VALIDATE $? "Removing MySQL data directories"
+# Remove MySQL data directory
+rm -rf /var/lib/mysql &>>$LOGFILE
+VALIDATE $? "Removing MySQL data directory"
 
-# Cleaning up unused packages
-echo "Cleaning up unused packages..." | tee -a $LOGFILE
-dnf autoremove -y &>>$LOGFILE
-VALIDATE $? "Cleaning up unused packages"
+# Remove MySQL configuration files
+rm -rf /etc/my.cnf /etc/mysql /etc/my.cnf.d /etc/mysql.d /etc/my.cnf.* &>>$LOGFILE
+VALIDATE $? "Removing MySQL configuration files"
 
-echo "MySQL Server removal completed successfully!" | tee -a $LOGFILE
+# Remove MySQL logs
+rm -rf /var/log/mysql* &>>$LOGFILE
+VALIDATE $? "Removing MySQL logs"
+
+# Optionally, remove any MySQL user accounts created
+userdel -r mysql &>>$LOGFILE
+VALIDATE $? "Removing MySQL user account"
+
+# Clean up dnf caches
+dnf clean all &>>$LOGFILE
+VALIDATE $? "Cleaning up DNF caches"
+
+# Remove MySQL root password entry from the system
+# This is optional and only if you wish to remove any saved credentials
+unset MYSQL_ROOT_PASSWORD &>>$LOGFILE
+VALIDATE $? "Removing MySQL root password from the environment"
+
+echo "MySQL has been completely removed from the system." &>>$LOGFILE
